5c9ccb3384c3e782b6d79cc5996d019209d6fcd7
Chynara <chynara.erkulova@gmail.com>
2020-11-15 15:07:55 +0300

    "favorite endpoint can't grab userId from local strategy"

--- models/project.js
+++ models/project.js
@@ -4,9 +4,9 @@ module.exports = (sequelize, DataTypes) => {
   Project.associate = function (models) {
     //Users.hasMany(models.Projects);
     Project.belongsToMany(models.User, { through: "UserProjects" });
-    Project.belongsTo(models.Organizations);
-    Project.belongsTo(models.Countries);
-    Project.belongsTo(models.Themes);
+    Project.belongsTo(models.Organization);
+    Project.belongsTo(models.Country);
+    Project.belongsTo(models.Theme);
   };
   return Project;
 };
--- routes/favorites.js
+++ routes/favorites.js
@@ -1,33 +1,30 @@
 var express = require("express");
 var router = express.Router();
 var models = require("../models");
+const passport = require("passport");
 
-//add a new project to users
-router.post("/", async (req, res) => {
-  const { ProjectId, UserId } = req.body;
+//add a new project to users in UserProjects model
+router.post(
+  "/",
+  passport.authenticate("local", { session: false }),
+  async (req, res) => {
+    const { UserId } = req.user.id; //grab id from token?
+    const { ProjectId } = req.body;
+    console.log(UserId);
 
-  try {
-    /*const project = await models.Project.findOne({
-      where: {
-        id: ProjectId,
-      },
-    });
-    //res.send(project);*/
-
-    const user = await models.User.findOne({
-      where: {
-        id: UserId,
-      },
-    });
-    //res.send(user);
-    /*const favorite = await user.addProject(project, {
-      through: { id: ProjectId },
-    });*/
-    const favorite = await user.addProject(ProjectId);
-    res.send(favorite);
-  } catch (err) {
-    res.status(500).send(err.message);
+    try {
+      const user = await models.User.findOne({
+        where: {
+          id: UserId,
+        },
+      });
+      const favorite = await user.addProject(ProjectId);
+      console.log(ProjectId);
+      res.send(favorite);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
   }
-});
+);
 
 module.exports = router;
