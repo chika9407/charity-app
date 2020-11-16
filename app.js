9ddcac9c0558728ff33b082c7c0d8cd08843daff
Chynara <chynara.erkulova@gmail.com>
2020-11-16 11:39:57 +0300

    req.user.id undefined..tried bodyParser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
+//var bodyParser = require("body-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
@@ -14,12 +15,15 @@ var favoritesRouter = require("./routes/favorites");
 var app = express();
 
 app.use(logger("dev"));
+//app.use(express.bodyParser());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 require("./routes/passportConfig"); //(passport);
+
 //app.use(passport.initialize());
 //app.use(passport.session());
 
