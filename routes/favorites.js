51558a26d463fe30913285cc1724a51c7c8df799
Chynara <chynara.erkulova@gmail.com>
2020-11-16 16:23:27 +0300

    getFavorites for Favorite/Dashboard page is working. Add to favorites still need work

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,16 +4,13 @@ var models = require("../models");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var jsonParser = bodyParser.json();
 
 //add a new project to users in UserProjects model
 router.post(
   "/",
-  passport.authenticate("local", { session: false }),
+  passport.authenticate("jwt", { session: false }),
   async (req, res) => {
-    console.log(typeof req.user.id);
-    const { UserId } = req.user.id; //grab id from token?
+    const UserId = req.user.id; //grab id from token
     const { ProjectId } = req.body;
     console.log(UserId);
     try {
@@ -25,6 +22,30 @@ router.post(
       const favorite = await user.addProject(ProjectId);
       console.log(ProjectId);
       res.send(favorite);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
+);
+
+//get projects for a specific user
+router.get(
+  "/projects",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const id = req.user.id;
+    //console.log(UserId);
+    //res.send(UserId);
+    //grab the user by id
+    try {
+      const user = await models.User.findOne({
+        where: {
+          id: id,
+        },
+        include: models.Project,
+      });
+      //const projects = await user.getProject();
+      res.send(user);
     } catch (err) {
       res.status(500).send(err.message);
     }
